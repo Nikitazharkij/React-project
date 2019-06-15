2f7f5af2d5d6bbc56a9d3d49d3befa6e27359ae7
Nikita Zharkij <kakdelanik@mail.ru>
2019-06-15 23:45:09 +0200

    Added horizontal scroll pics and state.js

--- src/App.js
+++ src/App.js
@@ -1,50 +1,113 @@
 import React from 'react';
 import './App.sass';
 import Header from './components/Header/Header';
+import addisAbaba from './img/Africa/AddisAbaba.jpg'
+import cairo from './img/Africa/Cairo.jpg'
+import agra from './img/Asia/Agra.jpg'
+import astana from './img/Asia/Astana.jpg'
+import amsterdam from './img/Europa/Amsterdam.jpg'
+import antalya from './img/Europa/Antalya.jpg'
 
 const App = (props) => {
   return (
-    <div className = "app-wrapper">
+    <div className="app-wrapper">
       <Header />
       <main>
         <div className="container profile-cities">
-          <div className="row ">
-              <div className="col-12 t">
+          <div className="row">
+              <div className="col-12 titleContinent">
                 <p>AFRICA (2)</p>
               </div>
           </div>
-          <div className="row s">
-            <div className="scroll-cities">
-              <div>
-                <img/>
+          <div className="row lineCities">
+            <div className="col-12 scrollCities">
+              <div className="picCityFrame">
+                <div>
+                  <img className="previewCity" src={addisAbaba} alt="AddisAbaba"/>
+                </div>
+                <div className="picCityText">
+                  <span>ADDIS ABABA</span>
+                </div>
+              </div>
+              <div className="picCityFrame">
+                <div>
+                  <img className="previewCity" src={cairo} alt="Cairo"/>
+                </div>
+                <div className="picCityText">
+                  <span>CAIRO</span>
+                </div>
+              </div>
+              <div className="picCityFrame">
+                <div>
+                  <img className="previewCity" src={addisAbaba} alt="AddisAbaba"/>
+                </div>
+                <div className="picCityText">
+                  <span>ADDIS ABABA</span>
+                </div>
+              </div>
+              <div className="picCityFrame">
+                <div>
+                  <img className="previewCity" src={cairo} alt="Cairo"/>
+                </div>
+                <div className="picCityText">
+                  <span>CAIRO</span>
+                </div>
               </div>
             </div>
           </div>
         </div>
         <div className="container profile-cities">
           <div className="row">
-              <div className="col-12 t">
+              <div className="col-12 ttitleContinent">
                 <p>ASIA (2)</p>
               </div>
           </div>
-          <div className="row">
-              <div className="col-6 s">
+          <div className="row lineCities">
+            <div className="scrollCities">
+              <div className="picCityFrame">
+                <div>
+                  <img className="previewCity" src={agra} alt="Agra"/>
+                </div>
+                <div className="picCityText">
+                  <span>AGRA</span>
+                </div>
               </div>
-              <div className="col-6 s">
+              <div className="picCityFrame">
+                <div>
+                  <img className="previewCity" src={astana} alt="Astana"/>
+                </div>
+                <div className="picCityText">
+                  <span>ASTANA</span>
+                </div>
               </div>
+            </div>
           </div>
         </div>
         <div className="container profile-cities">
           <div className="row">
-              <div className="col-12 t">
+              <div className="col-12 titleContinent">
                 <p>EUROPA (2)</p>
               </div>
           </div>
-          <div className="row">
-              <div className="col-6 s">
+          <div className="row lineCities">
+            <div className="scrollCities">
+              <div className="picCityFrame">
+                <div>
+                  <img className="previewCity" src={amsterdam} alt="Amsterdam"/>
+                </div>
+                <div className="picCityText">
+                  <span>AMSTERDAM</span>
+                </div>
               </div>
-              <div className="col-6 s">
+              <div className="picCityFrame">
+                <div>
+                  <img className="previewCity" src={antalya} alt="Antalya"/>
+                </div>
+                <div className="picCityText">
+                  <span>ANTALYA</span>
+                </div>
               </div>
+            </div>
           </div>
         </div>
       </main>
---
+++ src/redux/state.js
@@ -0,0 +1,31 @@
+let state = {
+    continent: {
+        africa: [
+        {id: 1, city: 'Addis Ababa', country: 'Ethiopia', src: './img/Africa/AddisAbaba.jpg'},
+        {id: 2, city: 'Cairo', country: 'Egipt', src: './img/Africa/Cairo.jpg'},
+        {id: 3, city: 'Dakar', country: 'Senegal', src: './img/Africa/Dakar.jpg'},
+        {id: 4, city: 'Accra', country: 'Ghana', src: './img/Africa/Ghana.jpg'}
+      ]
+    },
+
+    dialogsPage: {
+      dialogs: [
+        {id: 1, name: 'Dimych'},
+        {id: 2, name: 'Andrey'},
+        {id: 3, name: 'Sveta'},
+        {id: 4, name: 'Sasha'},
+        {id: 5, name: 'Victor'},
+        {id: 6, name: 'Valera'}
+      ],
+      messages: [
+        {id: 1, message: 'Hi'},
+        {id: 2, message: 'How is your project?'},
+        {id: 3, message: 'Yoy'},
+        {id: 4, message: 'Yoy'},
+        {id: 5, message: 'Yoy'}
+      ]
+    }
+}
+
+export default state;
+
